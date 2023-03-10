8d2e88fbe59424fc28811d020cc65745408e7dc3
BryanPool99 <bryanpool991@hotmail.com>
2023-03-09 19:45:42 -0500

    Test de prueba de clase Vehiculo

---
+++ Ejercicios/src/Herencia/TestCoche.java
@@ -0,0 +1,8 @@
+package Herencia;
+
+public class TestCoche {
+	public static void main(String[] args) {
+		Coche c=new Coche("TOYOTA", 2013, "001", 2);
+		System.out.println(c.toString());
+	}
+}
---
+++ Ejercicios/src/Herencia/TestMoto.java
@@ -0,0 +1,8 @@
+package Herencia;
+
+public class TestMoto {
+	public static void main(String[] args) {
+		Moto m=new Moto("ABC", 2020, "ZXY", 3);
+		System.out.println(m.toString());
+	}
+}

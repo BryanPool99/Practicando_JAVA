4b3b3be43cd93f0ad6bb702bdbe15cb62fb2e00b
BryanPool99 <bryanpool991@hotmail.com>
2023-03-09 19:44:41 -0500

    Test de prueba de clase Persona

---
+++ Ejercicios/src/Herencia/TestEstudiante.java
@@ -0,0 +1,8 @@
+package Herencia;
+
+public class TestEstudiante {
+	public static void main(String[] args) {
+		Estudiante e1=new Estudiante("Bryan", 23, "Jron Queti", 15.1);
+		System.out.println(e1.toString());
+	}
+}
---
+++ Ejercicios/src/Herencia/TestProfesor.java
@@ -0,0 +1,8 @@
+package Herencia;
+
+public class TestProfesor {
+	public static void main(String[] args) {
+		Profesor p1=new Profesor("Ivan", 47, "Jr. 123 SN", "Matematica II");
+		System.out.println(p1.toString());
+	}
+}

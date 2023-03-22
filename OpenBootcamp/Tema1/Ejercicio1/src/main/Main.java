2e1f6923122879e40425c0ece72453ae9d9d847e
BryanPool99 <bryanpool991@hotmail.com>
2023-03-22 18:37:04 -0500

    Ejercicio terminado de tema 1 openBootcamp

---
+++ OpenBootcamp/Tema1/Ejercicio1/src/main/Main.java
@@ -0,0 +1,28 @@
+
+package main;
+
+/*
+Para este primer ejercicio tendréis que realizar lo siguiente:
+
+-Crea un proyecto de Java desde 0
+
+-Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+
+-Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+
+-Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+*/
+public class Main {
+    public static void main(String[] args) {
+        int a=5;
+        long b=4L;
+        double c=4.95;
+        boolean isDev=true;
+        String user="Bryan Arredondo";
+        System.out.println("El valor de la variable a es:"+a);
+        System.out.println("El valor de la variable b es:"+b);
+        System.out.println("El valor de la variable c es:"+c);
+        System.out.println("El valor de la variable isDev es:"+isDev);
+        System.out.println("El valor de la variable user:"+user);
+    }
+}
